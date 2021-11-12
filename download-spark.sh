689411023887c14e58dd5ba4832659df647b23e2
ronald33 <ronald.dev1@gmail.com>
2021-11-11 23:45:50 -0500

    first commit

---
+++ download-spark.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://archive.apache.org/dist/spark/spark-2.1.1/spark-2.1.1-bin-hadoop2.6.tgz
+tar -xzvf spark-2.1.1-bin-hadoop2.6.tgz
---
+++ ingest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat etla{a..b} > geotrellis-spark-etl-assembly-2.2.0.jar
+rm etlaa etlab
+$(pwd)/spark-2.1.1-bin-hadoop2.6/bin/spark-submit \
+   --class geotrellis.spark.etl.MultibandIngest \
+   --master 'local[*]' \
+   --driver-memory 16G \
+   geotrellis-spark-etl-assembly-2.2.0.jar \
+   --input "file://$(pwd)/input.json" \
+   --output "file://$(pwd)/output.json" \
+   --backend-profiles "file://$(pwd)/backend-profiles.json"
